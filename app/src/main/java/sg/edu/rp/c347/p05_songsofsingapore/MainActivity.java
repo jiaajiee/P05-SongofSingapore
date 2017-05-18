bce84f9c17c66990aeecf47de585a9242bd5c7aa
15017274 <15017274@myrp.edu.sg>
2017-05-18 14:05:59 +0800

    jj

--- app/src/main/java/sg/edu/rp/c347/p05_songsofsingapore/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p05_songsofsingapore/MainActivity.java
@@ -2,12 +2,59 @@ package sg.edu.rp.c347.p05_songsofsingapore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert, btnShow;
+    EditText etTitle, etSinger, etYear;
+    RadioGroup rg;
+    ArrayList<String> al;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnShow = (Button) findViewById(R.id.btnShow);
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etSinger = (EditText) findViewById(R.id.etSinger);
+        etYear = (EditText) findViewById(R.id.etYear);
+
+        al = new ArrayList<String>();
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = etTitle.getText().toString();
+                String singer = etSinger.getText().toString();
+                int year = Integer.parseInt(etYear.getText().toString());
+                rg = (RadioGroup)findViewById(R.id.rg);
+                int selected = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(selected);
+
+                String data = title + singer + year + rb;
+
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long row_affected = dbh.insertSong(data);
+                dbh.close();
+
+                if (row_affected != -1){
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
