5ccc16cc80c65ceba702050a348f409cbc8a9f3f
15017274 <15017274@myrp.edu.sg>
2017-05-18 14:08:27 +0800

    jj

---
+++ app/src/main/java/sg/edu/rp/c347/p05_songsofsingapore/DBHelper.java
@@ -0,0 +1,49 @@
+package sg.edu.rp.c347.p05_songsofsingapore;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by 15017274 on 18/5/2017.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "song.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_SONG = "song";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_SONG_CONTENT = "song_content";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createSongTableSql = "CREATE TABLE " + TABLE_SONG + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_SONG_CONTENT + " TEXT ) ";
+        db.execSQL(createSongTableSql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SONG);
+        onCreate(db);
+    }
+
+    public long insertSong(String noteContent) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_SONG_CONTENT, noteContent);
+        long result = db.insert(TABLE_SONG, null, values);
+        db.close();
+        Log.d("SQL Insert",""+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
+}
---
+++ app/src/main/java/sg/edu/rp/c347/p05_songsofsingapore/Song.java
@@ -0,0 +1,43 @@
+package sg.edu.rp.c347.p05_songsofsingapore;
+
+/**
+ * Created by 15017274 on 18/5/2017.
+ */
+
+public class Song {
+    private int id;
+    private String title;
+    private  String singers;
+    private int year;
+    private int stars;
+
+    public Song(int id, String title, String singers, int year, int stars) {
+        this.id = id;
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+
+}
